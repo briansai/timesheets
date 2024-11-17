d42493cc08db6588f28be7ff6067bb19b796a4d6
Brian Sai <brianwsai@gmail.com>
2024-11-17 14:02:01 -0800

    Set file results to the side

--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -1,66 +1,85 @@
-'use client'
+"use client";
 
-import React, {useRef} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useRef } from "react";
+import { useDropzone } from "react-dropzone";
 
 function DropZone(props) {
-  const {required, name} = props; 
+  const { required, name } = props;
 
   const hiddenInputRef = useRef(null);
 
-  const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({
+  const { getRootProps, getInputProps, open, acceptedFiles } = useDropzone({
     onDrop: (incomingFiles) => {
       if (hiddenInputRef.current) {
-        // Note the specific way we need to munge the file into the hidden input
-        // https://stackoverflow.com/a/68182158/1068446
         const dataTransfer = new DataTransfer();
         incomingFiles.forEach((v) => {
           dataTransfer.items.add(v);
         });
         hiddenInputRef.current.files = dataTransfer.files;
       }
-    }
+    },
   });
 
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+  const files = acceptedFiles.map((file) => {
+    console.log(file);
+    return (
+      <li key={file.path}>
+        {file.path} - {file.size} bytes
+      </li>
+    );
+  });
 
   return (
     <div className="container">
-      <div {...getRootProps({className: 'dropzone'})}>
+      <div {...getRootProps({ className: "dropzone" })}>
         {/*
           Add a hidden file input 
           Best to use opacity 0, so that the required validation message will appear on form submission
         */}
-        <input type ="file" name={name} required={required} style ={{opacity: 0}} ref={hiddenInputRef}/>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here</p>
-        <button type="button" onClick={open}>
-          Open File Dialog
-        </button>
+        <div className="grid grid-rows-3 grid-flow-col gap-4">
+          <div className="row-span-3 ...">
+            <input
+              type="file"
+              name={name}
+              required={required}
+              style={{ opacity: 0 }}
+              ref={hiddenInputRef}
+            />
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some Excel Timesheets here</p>
+            <button
+              type="button"
+              onClick={open}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Open File Dialog
+            </button>
+          </div>
+
+          <div className="row-span-3">
+            <aside>
+              <h4>Files</h4>
+              <ul>{files}</ul>
+            </aside>
+          </div>
+        </div>
       </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
     </div>
   );
 }
 
+<form
+  onSubmit={(e) => {
+    e.preventDefault();
 
-<form onSubmit={(e) => {
-  e.preventDefault(); 
-  
-  // Now get the form data as you regularly would
-  const formData = new FormData(e.currentTarget);
-  const file =  formData.get("my-file");
-  alert(file.name); 
-}}>
-  <DropZone name ="my-file" required/>
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("my-file");
+
+    alert(file.name);
+  }}
+>
+  <DropZone name="my-file" required />
   <button type="submit">Submit</button>
-</form>
+</form>;
 
-export default DropZone
+export default DropZone;
